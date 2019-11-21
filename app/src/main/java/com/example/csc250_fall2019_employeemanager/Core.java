aa4d29c4cae11469b81a055055a18eee7d1d3ccb
jdcarlson07 <56281452+jdcarlson07@users.noreply.github.com>
2019-11-21 12:18:44 -0600

    updated with ListView, arrayLists and adapters

--- app/src/main/java/com/example/csc250_fall2019_employeemanager/Core.java
+++ app/src/main/java/com/example/csc250_fall2019_employeemanager/Core.java
@@ -7,5 +7,16 @@ public class Core
     public static Employee theEmployee = null;
     public static int myValue;
     public static ArrayList<Employee> theEmployees = new ArrayList<Employee>();
+    public static ArrayList<String> theEmployeeNames = new ArrayList<String>();
+    public static int employeeCounter = 0;
 
+    public static void generateNameList()
+    {
+       while(Core.employeeCounter < Core.theEmployees.size())
+        {
+            String temp = Core.theEmployees.get(Core.employeeCounter).toString();
+            Core.theEmployeeNames.add(temp);
+            Core.employeeCounter++;
+        }
+    }
 }
--- app/src/main/java/com/example/csc250_fall2019_employeemanager/EmployeeListActivity.java
+++ app/src/main/java/com/example/csc250_fall2019_employeemanager/EmployeeListActivity.java
@@ -3,23 +3,28 @@ package com.example.csc250_fall2019_employeemanager;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.ArrayAdapter;
 
 public class EmployeeListActivity extends AppCompatActivity
 {
-    TextView list;
+
+    ListView employeeList;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_employee_list);
 
-        this.list = this.findViewById(R.id.list);
+
+        this.employeeList = this.findViewById(R.id.employeeLV);
+
         if(Core.theEmployees.size() > 0)
         {
-            int lastItem = Core.theEmployees.size() - 1;
-            String stringOfLastObject = Core.theEmployees.get(lastItem).toString();
-            list.setText(stringOfLastObject);
+            Core.generateNameList();
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, Core.theEmployeeNames);
+            this.employeeList.setAdapter(adapter);
         }
     }
 }
